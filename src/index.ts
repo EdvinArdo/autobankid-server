fbba3b8a65c051dfd39e75924df0e48f82ba3edb
Edvin Ardö <ext.Edvin.Ardo@getswish.se>
2023-05-25 00:57:54 +0200

    Add request queue

--- src/index.ts
+++ src/index.ts
@@ -14,10 +14,51 @@ server.listen(process.env.PORT, () => {
   console.log("Server started");
 });
 
+interface SignRequest {
+  autostarttoken: string;
+  resolve: (value?: unknown) => void;
+}
+
+let isFirstRequest = true;
+const requestQueue: SignRequest[] = [];
+
 app.get("/api/autobankid", async (req, res) => {
   const { autostarttoken } = req.query;
   console.log(`Request with autostarttoken: ${autostarttoken}`);
-  await open(`bankid:///?autostarttoken=${autostarttoken}&redirect=null`);
-  setTimeout(() => open(`autobankidauth://`), 1000);
+
+  if (typeof autostarttoken !== "string") {
+    res.status(400).send("autostarttoken is not a string");
+    return;
+  }
+
+  await new Promise((resolve) => {
+    requestQueue.push({ autostarttoken, resolve });
+    if (requestQueue.length === 1) {
+      handleSignRequest(requestQueue[0]);
+    }
+  });
+
   res.send();
 });
+
+async function handleSignRequest(signRequest: SignRequest) {
+  await openAndSignBankId(signRequest.autostarttoken);
+  signRequest.resolve("Resolved");
+
+  requestQueue.shift();
+  if (requestQueue.length > 0) {
+    handleSignRequest(requestQueue[0]);
+  }
+}
+
+async function openAndSignBankId(autostarttoken: string) {
+  await open(`bankid:///?autostarttoken=${autostarttoken}&redirect=null`);
+  await sleep(isFirstRequest ? 3000 : 1000);
+  isFirstRequest = false;
+  await open(`autobankidauth://`);
+  await sleep(1000);
+}
+
+function sleep(ms: number) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
